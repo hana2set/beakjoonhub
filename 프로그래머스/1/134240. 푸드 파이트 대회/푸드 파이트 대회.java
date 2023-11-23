895ae54b5bbf520b8151c33bd8aff7f13819d3aa
hana2set <97689567+hana2set@users.noreply.github.com>
2023-11-23 11:11:48 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 13.82 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < food.length; i++) {
-            int count = food[i]/2;
-            sb.append(String.valueOf(i).repeat(count));
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
+            }
         }
-        answer = sb.toString() + "0" + sb.reverse().toString();
         return answer;
     }
 }
