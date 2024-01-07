8835d94cb038064212c152d1c64077d9c528b7e0
hana2set <97689567+hana2set@users.noreply.github.com>
2024-01-07 22:10:55 +0900

    Update 모음 사전.java

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -27,3 +27,37 @@ class Solution {
         return answer;
     }
 }
+
+
+// 다른사람 풀이1
+// class Solution {
+//     public int solution(String word) {
+//         int answer = 0, per = 3905;
+//         for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
+//         return answer;
+//     }
+// }
+// per : 모든 낱말의 갯수 => 시그마 5^n
+// 각 자리 사이에 시그마 5^n개의 문자열이 있음.
+// 나누기(/)연산으로 시그마 5^(n-1) 찾음
+// +1로 자기자신 계산
+
+
+// 다른사람 풀이2
+// import java.util.*;
+// class Solution {
+//     List<String> list = new ArrayList<>();
+//     void dfs(String str, int len) {
+//         if(len > 5) return;
+//         list.add(str);
+//         for(int i = 0; i < 5; i++) dfs(str + "AEIOU".charAt(i), len + 1);
+//     }
+//     public int solution(String word) {
+//         dfs("", 0);
+//         return list.indexOf(word);
+//     }
+// }
+// 문자열 하나씩 더하면서 사전 완성
+// 인덱스 계산
+
+
