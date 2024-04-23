f319a87cd0da18ecec6d61031556b41bfea4e9ce
hana2set <97689567+hana2set@users.noreply.github.com>
2024-04-23 10:50:58 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.99 ms, Memory: 72.7 MB -BaekjoonHub

--- Java/프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ Java/프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -6,26 +6,19 @@ class Solution {
         
         // col 번째 컬럼값 기준 오름차순 정렬
         // 같으면 첫번째 컬럼값으로 내림차순 정렬
-        Arrays.sort(data, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[col-1] == o2[col-1] ? o2[0] - o1[0] : o1[col-1] - o2[col-1];
-            }
-        });
+        Arrays.sort(data, (o1, o2) -> o1[col-1] == o2[col-1] ? o2[0] - o1[0] : o1[col-1] - o2[col-1]);
         
         // S_i = (i 번째 행의 튜플)에 대해 (각 컬럼의 값)을 (i) 로 나눈 (나머지들의 합)
         int[] S_i = new int[data.length];
-        for (int i = 0; i < data.length; i++) {
+        for (int i = row_begin-1; i < row_end; i++) {
             for (int j = 0; j < data[i].length; j++) {
                 S_i[i] += data[i][j] % (i+1);
             }
-        }
-        
-        // row_begin ≤ i ≤ row_end 인 모든 S_i를 누적하여 bitwise XOR 한 값을 해시 값으로서 반환 (^)
-        answer = S_i[row_begin-1];
-        for (int i = row_begin; i < row_end; i++) {
+            
+            // row_begin ≤ i ≤ row_end 인 모든 S_i를 누적하여 bitwise XOR 한 값을 해시 값으로서 반환 (^)
             answer ^= S_i[i];
         }
+        
         
         // System.out.println(Arrays.deepToString(data));
         // System.out.println(Arrays.toString(S_i));
