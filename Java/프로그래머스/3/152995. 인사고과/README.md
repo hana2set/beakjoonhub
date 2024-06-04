6788f08ef08b4522964caf92e579f7b3012726b7
hana2set <97689567+hana2set@users.noreply.github.com>
2024-06-04 20:16:54 +0900

    [level 3] Title: 인사고과, Time: 70.65 ms, Memory: 113 MB -BaekjoonHub

--- Java/프로그래머스/3/152995. 인사고과/README.md
+++ Java/프로그래머스/3/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [level 3] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995#) 
 
 ### 성능 요약
 
-메모리: 124 MB, 시간: 424.16 ms
+메모리: 113 MB, 시간: 70.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 18:58:21
+2024년 06월 04일 20:16:52
 
 ### 문제 설명
 
--- Java/프로그래머스/3/152995. 인사고과/인사고과.java
+++ Java/프로그래머스/3/152995. 인사고과/인사고과.java
@@ -23,43 +23,68 @@ class Solution {
     }
     
     public int solution(int[][] scores) {
-        int answer = 0;
+//         int answer = 0;
         
-        List<Person> list = new ArrayList<>();
+//         List<Person> list = new ArrayList<>();
         
-        for (int i = 0; i < scores.length; i++) {
-            list.add(new Person(i, scores[i][0], scores[i][1]));
-        }
+//         for (int i = 0; i < scores.length; i++) {
+//             list.add(new Person(i, scores[i][0], scores[i][1]));
+//         }
         
-        list.sort((e1, e2) -> {
-            if (e2.sumScore == e1.sumScore) {
-                return e1.index - e2.index;
-            }
-            return e2.sumScore - e1.sumScore;
-        });
+//         list.sort((e1, e2) -> {
+//             if (e2.sumScore == e1.sumScore) {
+//                 return e1.index - e2.index;
+//             }
+//             return e2.sumScore - e1.sumScore;
+//         });
         
-        int skipCount = 0;
-        for (int i = 0; i < list.size(); i++) {
-            boolean isSkip = false;
-            for (int j = 0; j < i; j++) {
-                if (list.get(j).p1 > list.get(i).p1
-                   && list.get(j).p2 > list.get(i).p2) {
-                    skipCount++;
-                    isSkip = true;
-                    break;
-                }
-            }
+//         int skipCount = 0;
+//         for (int i = 0; i < list.size(); i++) {
+//             boolean isSkip = false;
+//             for (int j = 0; j < i; j++) {
+//                 if (list.get(j).p1 > list.get(i).p1
+//                    && list.get(j).p2 > list.get(i).p2) {
+//                     skipCount++;
+//                     isSkip = true;
+//                     break;
+//                 }
+//             }
             
         
-            if (list.get(i).index == 0) {
-                if (isSkip == true) {
-                    return -1;
-                }
+//             if (list.get(i).index == 0) {
+//                 if (isSkip == true) {
+//                     return -1;
+//                 }
                 
-                return i-skipCount+1;
+//                 return i-skipCount+1;
+//             }
+//         }
+        
+//         return -1;
+        
+        
+        
+        
+        // 다른사람 답안
+        int[] s = scores[0];
+        // 앞숫자 내림차순, 뒷숫자 오름차순
+        Arrays.sort(scores, (s1, s2) -> s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0]);
+        ArrayList<Integer> arr = new ArrayList<>();
+        int max = 0, ans = 1;
+        // 내림차순임으로 앞숫자는 항상 큼으로, 앞에서부터 뒷숫자의 max를 구하면
+        // max만 비교하면 됨(max 보다 작을 경우 해당 score보다 두점수가 큰 경우가 무조건 있음.)
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s))
+                    return -1;
+            }
+            else {
+                max = Math.max(max, score[1]);
+                if(s[0] + s[1] < score[0] + score[1])
+                    ans++;
             }
         }
+        return ans;
         
-        return -1;
     }
 }
