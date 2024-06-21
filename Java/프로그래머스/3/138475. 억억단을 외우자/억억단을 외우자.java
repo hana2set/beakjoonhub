a65a50226bd843cb908f3bbb0262618c1019bdde
hana2set <97689567+hana2set@users.noreply.github.com>
2024-06-21 23:34:47 +0900

    [level 3] Title: 억억단을 외우자, Time: 511.91 ms, Memory: 148 MB -BaekjoonHub

--- Java/프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ Java/프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,63 +1,27 @@
-import java.util.*;
-
 class Solution {
-    public static int[] solution(int e, int[] starts) {
-        int[] answer = new int[starts.length];
+    public int[] solution(int e, int[] starts) {
+        int[] res = new int[starts.length];
 
-        /*
-         * "e 부터" 시작하여 1까지 최대 약수의 개수를 저장함
-         * [0] : 최대 약수의 개수
-         * [1] : 최대 약수의 위치
-         * */
-        int[][] store = new int[e+1][2];
-        store[e][0] = primeCount(e);
-        store[e][1] = e;
-        for(int i=e-1; i>=1; i--){
-            int cnt = primeCount(i);
-
-            // 최대 약수 갱신
-            if(cnt >= store[i+1][0]){
-                store[i][0] = cnt;
-                store[i][1] = i;
-            }else{
-                store[i][0] = store[i+1][0];
-                store[i][1] = store[i+1][1];
+        int[] count = new int[e + 1];
+        for (int i = 1; i <= e; i++) {
+            for (int j = 1; j <= e / i; j++) {
+                count[i * j]++;
             }
         }
 
-        for (int i = 0; i < starts.length; i++) {
-            int star = starts[i];
-            answer[i] = store[star][1];
-        }
-
-        return answer;
-    }
-
-    // 소수의 개수
-    private static int primeCount(int n) {
-        /*
-         * primes의 key는 밑이고, value는 지수
-         * 2³일 경우 key : 2, value : 3
-         * 약수의 개수를 구하는 공식을 사용하기 위해, primes에 지수를 저장
-         * */
-        Map<Integer, Integer> primes = new HashMap<>();
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            while (n % i == 0) {
-                primes.put(i, primes.getOrDefault(i, 0) + 1);
-                n /= i;
+        int[] max = new int[e + 1];
+        max[e] = e;
+        for (int i = e - 1; i >= 1; i--) {
+            if (count[i] >= count[max[i + 1]]) {
+                max[i] = i;
+            } else {
+                max[i] = max[i + 1];
             }
         }
 
-        if (n != 1) {
-            primes.put(n, primes.getOrDefault(n, 0) + 1);
+        for (int i = 0; i < res.length; i++) {
+            res[i] = max[starts[i]];
         }
-
-        int count = 1;
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(primes.entrySet());
-        for (Map.Entry<Integer, Integer> entry : list) {
-            count *= (entry.getValue() + 1);
-        }
-        return count;
+        return res;
     }
 }
