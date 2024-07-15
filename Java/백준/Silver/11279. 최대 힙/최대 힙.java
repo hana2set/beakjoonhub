8c84c9bf6c6aef39b895bddae1d74da8ee25f259
hana2set <97689567+hana2set@users.noreply.github.com>
2024-07-15 11:11:24 +0900

    [Silver II] Title: 최대 힙, Time: 280 ms, Memory: 26920 KB -BaekjoonHub

--- Java/백준/Silver/11279. 최대 힙/최대 힙.java
+++ Java/백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,24 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt();
-        PriorityQueue<Long> q = new PriorityQueue(Collections.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue(Collections.reverseOrder());
 
         while (N-- > 0) {
-            long num = sc.nextLong();
+            int num = Integer.parseInt(br.readLine());
 
             if (num == 0) {
-                sb.append(q.isEmpty() ? 0 : q.poll());
-                sb.append("\n");
+                bw.write(String.valueOf(q.isEmpty() ? 0 : q.poll()));
+                bw.write("\n");
             } else {
                 q.add(num);
             }
         }
 
-        System.out.println(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
