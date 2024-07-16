f8a2eaff4a331cf751b13031d548f6242935fb1e
hana2set <97689567+hana2set@users.noreply.github.com>
2024-07-16 14:17:18 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 27176 KB -BaekjoonHub

---
+++ Java/백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue();
+
+        while (N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                bw.write(String.valueOf(q.isEmpty() ? 0 : q.poll()));
+                bw.write("\n");
+            } else {
+                q.add(num);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
