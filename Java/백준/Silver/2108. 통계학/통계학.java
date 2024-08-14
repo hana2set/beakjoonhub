c3bdf5572120a16f64bf2c11a7a555e4f6a0afd1
hana2set <97689567+hana2set@users.noreply.github.com>
2024-08-14 14:52:28 +0900

    [Silver III] Title: 통계학, Time: 500 ms, Memory: 51920 KB -BaekjoonHub

---
+++ Java/백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+        int modeCount = 0;
+        int modeValue = 0;
+
+        boolean isSecondMode = false;
+        int refeat = 1;
+        for (int i = 0; i < N; i++) {
+            sum += arr[i];
+
+            if (i==0) continue;
+            // 값이 같으면 횟수 추가
+            if (arr[i-1] == arr[i]) {
+                refeat++;
+                continue;
+            }
+
+            //값이 다르면 초기화
+            if (modeCount < refeat) {
+                isSecondMode = false;
+                modeValue = arr[i-1];
+                modeCount = refeat;
+            } else if (isSecondMode == false && modeCount == refeat) {
+                isSecondMode = true;
+                modeValue = arr[i-1];
+                modeCount = refeat;
+            }
+
+            refeat = 1;
+
+        }
+
+        if (modeCount < refeat) {
+            modeValue = arr[N-1];
+        } else if (isSecondMode == false && modeCount == refeat) {
+            modeValue = arr[N-1];
+        }
+
+//        산술평균 : N개의 수들의 합을 N으로 나눈 값. 소수점 이하 첫째 자리에서 반올림한 값
+        System.out.println(Math.round((double)sum/N));
+
+//        중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값. N은 홀수라고 가정됨
+        System.out.println(arr[N/2]);
+
+//        최빈값 : N개의 수들 중 가장 많이 나타나는 값
+        System.out.println(modeValue);
+
+//        범위 : N개의 수들 중 최댓값과 최솟값의 차이
+        System.out.println(arr[N-1] - arr[0]);
+
+    }
+
+}
