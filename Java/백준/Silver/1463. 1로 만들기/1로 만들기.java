1c69526c9cd44f161fdc8b9919d1def2f68055bb
hana2set <97689567+hana2set@users.noreply.github.com>
2024-09-11 16:30:01 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 17980 KB -BaekjoonHub

--- Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,11 +11,10 @@ class Main {
         dp[1] = 0;
 
         for (int i = 2; i <= N; i++) {
-            dp[i] = Integer.MAX_VALUE;
-
+            dp[i] = dp[i-1] + 1;
+            
             if (i % 2 == 0) dp[i] = Math.min(dp[i/2] + 1, dp[i]);
             if (i % 3 == 0) dp[i] = Math.min(dp[i/3] + 1, dp[i]);
-            dp[i] = Math.min(dp[i-1] + 1, dp[i]);
         }
 
         System.out.println(dp[N]);
