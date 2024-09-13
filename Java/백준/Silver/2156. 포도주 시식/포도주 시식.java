025d7756ec4d8bfb17095c1a613248ab65f841bb
hana2set <97689567+hana2set@users.noreply.github.com>
2024-09-13 17:53:11 +0900

    [Silver I] Title: 포도주 시식, Time: 120 ms, Memory: 14852 KB -BaekjoonHub

--- Java/백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ Java/백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -5,35 +5,34 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        if (N == 1) {
-            System.out.println(Integer.parseInt(br.readLine()));
-            return;
-        }
-
         int[] drink = new int[N];
         for (int i = 0; i < N; i++) {
             drink[i] = Integer.parseInt(br.readLine());
         }
 
-
         // 현재 포도주를 마실 수 있는지는 2 step 전 마신지 여부가 결정
         // 즉, 총 3 step이 영향을 받음으로 거기서의 최대값을 DP로 계산하면 확장할 수 있음
-        // 현재 포도주를 마셨을때, 안마셨을때의 최대값을 DP로 출력
-        // -> 현재 포도주 마셨을때 최대값 = Math.max(2step 전 안마신 포도주값 + 이전과 현재의 합, 1step 전 안마신 + 현재)
-        // -> 현재 포도주 안마셨을때 최대값 = Math.max(1step 전 마셨을때 최대값, 2step 전 마셨을때 최대값) <- 2 step 중 한번은 안마셔야함
-        int[][] dp = new int[N][2]; //0: 마심, 1: 안마심
+        // -> 현재 포도주 최대값 = Math.max(현재 안마시기, 1step 전 안마시기, 2step 전 안마시기)
+        //                    = Math.max(1step 전 최대값, 2step 전 최대값 + 현재, 3step 최대값 + 이전과 현재)
+        int[] dp = new int[N];
 
-        dp[0][0] = drink[0];
-        dp[1][0] = drink[1] + dp[0][0];
-        dp[1][1] = dp[0][0];
+        dp[0] = drink[0];
+        if (N > 1) dp[1] = dp[0] + drink[1];
+        if (N > 2) dp[2] =
+                Math.max(dp[1],
+                        Math.max(dp[0] + drink[2], drink[2]+drink[1])
+                );
 
-        for (int i = 2; i < N; i++) {
-            dp[i][0] = Math.max(drink[i] + dp[i-1][1], drink[i] + drink[i-1] + dp[i-2][1]);
-            dp[i][1] = Math.max(dp[i-1][0], dp[i-2][0]);
+        for (int i = 3; i < N; i++) {
+            dp[i] = Math.max(dp[i-1],
+                    Math.max(dp[i-2] + drink[i],
+                            dp[i-3] + drink[i-1] + drink[i]
+                    )
+            );
         }
 
 
-        System.out.println(Math.max(dp[N-1][0],dp[N-1][1]));
+        System.out.println(dp[N-1]);
     }
 
 }
