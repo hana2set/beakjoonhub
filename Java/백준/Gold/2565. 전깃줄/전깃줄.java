340732b727a3ee7a1f8c87166256d060c704e724
hana2set <97689567+hana2set@users.noreply.github.com>
2024-09-23 18:50:15 +0900

    [Gold V] Title: 전깃줄, Time: 108 ms, Memory: 14572 KB -BaekjoonHub

--- Java/백준/Gold/2565. 전깃줄/전깃줄.java
+++ Java/백준/Gold/2565. 전깃줄/전깃줄.java
@@ -6,34 +6,28 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        List<int[]> lines = new ArrayList<>();
+        int[][] lines = new int[N][2];
 
         for (int i = 0; i < N; i++) { //전깃줄 초기화
-            lines.add(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray());
+            lines[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
         // 같은 위치에 두 개 이상의 전깃줄이 연결될 수 없음으로
         // A로 정렬 후 나타나는 B의 수열의 증가하는 최장길이부분수열을 구하면 그것이 최솟값. 11053 LIS
         // B 에 대한 DP 계산
-        lines.sort(Comparator.comparingInt(a -> a[0]));
-
-        List<Integer> B = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            B.add(lines.get(i)[1]);
-        }
+        Arrays.sort(lines, Comparator.comparingInt(a -> a[0]));
 
         // dp : 해당 줄에 대한 LIS 길이 값
         int[] dp = new int[N];
         int result = 0;
 
         for (int i = 0; i < N; i++) {
-            int max = 1;
+            dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (B.get(j) < B.get(i)) max = Math.max(dp[j]+1, max);
+                if (lines[j][1] < lines[i][1]) dp[i] = Math.max(dp[j]+1, dp[i]);
             }
 
-            dp[i] = max;
-            result = Math.max(result, max);
+            result = Math.max(result, dp[i]);
         }
 
         // 없애야 되는 전깃줄 개수 출력
