0bccfe4d256cdf09d480a9fed46948cdb44840bf
hana2set <97689567+hana2set@users.noreply.github.com>
2024-09-26 16:59:12 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 824 ms, Memory: 115856 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ Java/백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -3,24 +3,45 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str = br.readLine();
         char[] chars = str.toCharArray();
         int N = Integer.parseInt(br.readLine());
-        while (N-- > 0) {
-            String[] input = br.readLine().split(" ");
-            char target = input[0].charAt(0);
-            int start = Integer.parseInt(input[1]);
-            int end = Integer.parseInt(input[2]);
-            int count = 0;
 
-            for (int i = start; i <= end; i++) {
-                if (target == chars[i]) count++;
+        // 문제수가 많아지면 직접 계산하는데에 절대적 시간이 길어짐
+        // 인덱스 별 알파벳 등장 횟수의 합을 미리 계산해두면 문제수에 따른 답을 빨리 도출 가능
+        int[][] count = new int[26][chars.length]; // [알파벳수][문자열길이]
+
+        count[chars[0] - 'a'][0]++;
+
+        for (int i = 1; i < chars.length; i++) {
+//            int addNum = chars[i] - 'a';
+//            for (int j = i; j < chars.length; j++) {
+//                count[addNum][j]++; // 나온 숫자 뒤 전체 +1
+//            }
+
+            for (int j = 0; j < 26; j++) {
+                count[j][i] = count[j][i-1];
             }
 
-            System.out.println(count);
+            count[chars[i] - 'a'][i]++;
         }
 
+        while (N-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int target = input[0].charAt(0) - 'a';
+            int start = Integer.parseInt(input[1]);
+            int end = Integer.parseInt(input[2]);
+
+            int result = count[target][end];
+            if (start != 0) result -= count[target][start-1];
+
+            bw.append(result+"\n");
+        }
+
+        bw.close();
+        br.close();
     }
 
 }
