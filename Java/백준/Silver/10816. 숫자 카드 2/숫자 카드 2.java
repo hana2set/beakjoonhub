5c154803e8bf28f0df2a7b0611e1899b07a810d9
hana2set <97689567+hana2set@users.noreply.github.com>
2024-10-16 18:16:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1084 ms, Memory: 191072 KB -BaekjoonHub

---
+++ Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 초기화
+        int N = Integer.parseInt(br.readLine());
+        int[] seq = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int M = Integer.parseInt(br.readLine());
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            map.computeIfAbsent(seq[i], a -> 0);
+            map.put(seq[i], map.get(seq[i])+1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            sb.append(map.get(input[i]) == null ? 0 : map.get(input[i])).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+
+    }
+
+}
